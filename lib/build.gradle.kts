4ca939498a1657118659a9566edb47a2f4d55034
Paul Harrison <paul.harrison@manchester.ac.uk>
2022-10-11 18:34:26 +0100

    Initial Commit

---
+++ lib/build.gradle.kts
@@ -0,0 +1,31 @@
+
+plugins {
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+}
+group = "org.orph2020.pst"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+    mavenLocal()
+}
+java {
+    withSourcesJar()
+}
+dependencies {
+    // Use JUnit Jupiter for testing.
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:31.0.1-jre")
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
