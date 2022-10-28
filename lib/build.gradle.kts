b37c8a162072b4d6eb69566bf03a93f8058186ca
Paul Harrison <paul.harrison@manchester.ac.uk>
2022-10-28 11:08:22 +0100

    integrate with central build logic

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -15,14 +15,10 @@ java {
     withSourcesJar()
 }
 dependencies {
+    api("org.javastro.ivoa.dm:proposaldm:0.2-SNAPSHOT:quarkus")//IMPL special dependency that does not include the
     // Use JUnit Jupiter for testing.
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.0.1-jre")
 }
 
 tasks.named<Test>("test") {
