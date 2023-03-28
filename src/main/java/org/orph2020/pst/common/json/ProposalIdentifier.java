64c2335d2c82fb57e50990b8fd5a83c9c5240e83
Darren Walker <darrenwalker1@hotmail.co.uk>
2023-03-28 15:58:51 +0100

    add ProposalIdentifier class to identify ObservingProposals that use a String key ('code') rather than a Long key ('id')

--- src/main/java/org/orph2020/pst/common/json/ProposalIdentifier.java
+++ src/main/java/org/orph2020/pst/common/json/ProposalIdentifier.java
@@ -1,2 +1,16 @@
-package org.orph2020.pst.common.json;public class ProposalIdentifier {
+package org.orph2020.pst.common.json;
+
+public class ProposalIdentifier {
+
+    public String code;
+    public String title;
+
+    public ProposalIdentifier(String code, String title) {
+        this.code = code;
+        this.title = title;
+    }
+
+    public ProposalIdentifier() {
+
+    }
 }
