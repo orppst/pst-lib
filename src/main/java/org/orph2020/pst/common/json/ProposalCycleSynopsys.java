799f9c1154517d8eadc3ced826585dd4cb3375ac
Allan England <allan.england@manchester.ac.uk>
2025-10-08 09:23:59 +0100

    Change proposal cycle dates to synopsys

--- src/main/java/org/orph2020/pst/common/json/ProposalCycleSynopsys.java
+++ src/main/java/org/orph2020/pst/common/json/ProposalCycleSynopsys.java
@@ -4,21 +4,23 @@ import org.ivoa.dm.proposal.management.Observatory;
 
 import java.util.Date;
 
-public class ProposalCycleDates {
+public class ProposalCycleSynopsys {
     public String title;
+    public String code;
     public Date submissionDeadline;
     public Date observationSessionStart;
     public Date observationSessionEnd;
     public Observatory observatory;
 
-    public ProposalCycleDates(String title, Date submissionDeadline, Date observationSessionStart, Date observationSessionEnd, Observatory observatory)
+    public ProposalCycleSynopsys(String title, String code, Date submissionDeadline, Date observationSessionStart, Date observationSessionEnd, Observatory observatory)
     {
         this.title = title;
+        this.code = code;
         this.submissionDeadline = submissionDeadline;
         this.observationSessionStart = observationSessionStart;
         this.observationSessionEnd = observationSessionEnd;
         this.observatory = observatory;
     }
 
-    public ProposalCycleDates() {}
+    public ProposalCycleSynopsys() {}
 }
