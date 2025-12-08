cb8798cbfe41f37eaed9ac754cfb304a11f707af
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-12-08 09:26:22 +0000

    add in the UKSRC repo to pick up some SNAPSHOTS
    we have more control of what is going on if SNAPSHOTS are in our repository,
    rather than maven central.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,29 @@
-
-rootProject.name = "pst-lib"
-includeBuild("../build-logic")
+pluginManagement {
+    includeBuild("../build-logic")
+}
 
 dependencyResolutionManagement {
+//this does not always seem to be inherited?
     repositories {
         mavenCentral()
         mavenLocal()
         /*
-        add this repository to pick up the SNAPSHOT version of the IVOA base library - in the future this
+        add this repository to pick up the SNAPSHOT version of the IVOA base library - in the future  this
         will not be necessary when this library is released as a non-SNAPSHOT version.
          */
         maven {
             url= uri("https://central.sonatype.com/repository/maven-snapshots/")
         }
+        maven {
+            url= uri("https://repo.dev.uksrc.org/repository/maven-snapshots/")
+        }
+
 
     }
-
 }
+
+
+rootProject.name = "pst-lib"
+
+
+
