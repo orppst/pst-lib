cb76a00e0d12bf1dd0f3b0a7bb18d4f0c13e6c1c
Darren Walker <darrenwalker1@hotmail.co.uk>
2026-09-01 12:39:57 +0100

    add 'minChar' and 'maxChar' members to the DTO for observatory-specific questions

--- src/main/java/org/orph2020/pst/common/json/ObservatoryQuestion.java
+++ src/main/java/org/orph2020/pst/common/json/ObservatoryQuestion.java
@@ -6,14 +6,19 @@ public class ObservatoryQuestion {
     public String description;
     public String query;
     public String response;
+    public int minChar;
+    public int maxChar;
 
     public ObservatoryQuestion() {}
 
-    public ObservatoryQuestion(int id, String label, String description, String query, String response) {
+    public ObservatoryQuestion(int id, String label, String description, String query, String response,
+                               int minChar, int maxChar) {
         this.id = id;
         this.label = label;
         this.description = description;
         this.query = query;
         this.response = response;
+        this.minChar = minChar;
+        this.maxChar = maxChar;
     }
 }
